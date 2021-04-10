b03b32b0ede4074641f052daa4fee598e3b14a5a
dmitrisonnic <dmitrisonnic@gmail.com>
2021-04-10 16:09:41 +0300

    added  scss and fix navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
-const Header =()=>{
-    
-   return <header className='header'>
-    <img src='https://cdn.dribbble.com/users/10882/screenshots/15172621/media/cd2246d5d0f54f9a4316bd4d276764b2.png?compress=1&resize=400x300'/>
-  </header>
-}
-export default Header
+import { Button, Dropdown } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img src="https://cdn.dribbble.com/users/10882/screenshots/15172621/media/cd2246d5d0f54f9a4316bd4d276764b2.png?compress=1&resize=400x300" />
+      </div>
+      <div className="header_navigation">
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Button type="button" class="btn btn-primary">
+          Primary
+        </Button>
+        <a href="#">home</a>
+        <a href="#">News Feed</a>
+        <a href="#">Mesage</a>
+      </div>
+    </div>
+  );
+};
+export default Header;
